a14380ead5c06beec9501d48ee096e88bfb38f21
Brandon <bwkong88@gmail.com>
2024-06-19 15:26:57 -0400

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
-      {/* Add content for Home component */}
+      <h2 style={{ color: 'blue' }}>Welcome to My Portfolio</h2>
+      <p style={{ color: '#333' }}>Explore my projects, skills, and more!</p>
+      {/* Add more content with inline styles */}
     </div>
   );
 }
